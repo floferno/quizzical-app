faf329855a384be32e239a97677a8de383d3860a
Olfia R <58562757+floferno@users.noreply.github.com>
2022-10-07 14:14:54 +0800

    Update Homepage.js

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -3,10 +3,9 @@ import React from 'react'
 export default function Homepage(props) {
     return (
         <div className="homepage">
-            <h1 className="homepage_title">Harta, Tahta, Trivia</h1>
+            <h1 className="homepage_title">Quizzical</h1>
             <div className="homepage_subtitle">
-                <p>No one cares about your harta and tahta until you try our trivia quiz!</p>
-                <p>(Joking, bro, but not really!)</p>
+                <p>"Let's get quizzical!" Dua Lipa said.</p>
             </div>
             <button className="button" onClick={() => props.start()}>Start</button>
         </div>
